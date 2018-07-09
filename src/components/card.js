d7666b93fca7b75069c45864cc1ce4e8315265d9
Ana Lisnic <ana.lisnnic@gmail.com>
2018-07-09 19:43:07 +0300

    built first clas componenet for inputs container

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Input from './input';
+
+class Card extends Component {
+    render () {
+        return(
+            <div className="card">
+            { Input('color') }
+            { Input('plural noun') }
+            </div>
+        )
+    }
+}
+
+export default Card;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+        <h1>Bottaga Madlibs</h1>
+        <p>Fill out the fields below ang cilck the generate button in order to see the story.</p>
+        </div>
+    )
+}
+
+export default Header;
